aec62654adbc6ce446d207c4313c73e3ea322142
vijayarajsubramani <vijayarajsubramani@gmail.com>
2021-03-25 15:03:09 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input'
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {environment} from '../environments/environment'
+import {AngularFireModule} from 'angularfire2'
+import {AngularFirestoreModule} from 'angularfire2/firestore'
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    MatInputModule
+    BrowserAnimationsModule,FlexLayoutModule,FormsModule,ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
+    
     
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD_rBMhVMN4JWQ_iQIi8JCY-69eJQPYv9c",
+    authDomain: "chat-bot-42e38.firebaseapp.com",
+    projectId: "chat-bot-42e38",
+    storageBucket: "chat-bot-42e38.appspot.com",
+    messagingSenderId: "892066983300",
+    appId: "1:892066983300:web:7e9770add6f82a9c1481d1",
+    measurementId: "G-BBQWD3B7RW"
+  }
 };
 
 /*
