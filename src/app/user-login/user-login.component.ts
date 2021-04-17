4f4f352c200dc2730ba1fe2239cd1887eb5215dc
vijayarajsubramani <vijayarajsubramani@gmail.com>
2021-04-17 13:02:50 +0530

    commit

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -4,11 +4,12 @@ import { GoogleAuthServiceService } from '../service/google-auth-service.service
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
+  providers:[GoogleAuthServiceService]
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor(private auth:GoogleAuthServiceService) { 
+  constructor(public auth:GoogleAuthServiceService) { 
     
   }
 
